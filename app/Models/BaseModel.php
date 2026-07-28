d55359aa1e12ca0ee15c6d356eaa36f9848ab87e
Deft Valian Exanova <deftvalian2411@gmail.com>
2026-07-28 08:23:15 +0700

    update: welcome dashboard ui

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 abstract class BaseModel extends Model
 {
@@ -14,4 +15,21 @@ abstract class BaseModel extends Model
      * @var array
      */
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (auth()->check() && Schema::hasColumn($model->getTable(), 'created_by')) {
+                $model->created_by = auth()->id();
+            }
+        });
+
+        static::updating(function ($model) {
+            if (auth()->check() && Schema::hasColumn($model->getTable(), 'updated_by')) {
+                $model->updated_by = auth()->id();
+            }
+        });
+    }
 }

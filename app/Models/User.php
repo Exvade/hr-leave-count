4ab5ddf4c9bf40657ece47bc4b43be40ca3c3694
Deft Valian Exanova <deftvalian2411@gmail.com>
2026-07-28 08:44:18 +0700

    style: fix laravel default files formatting

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,9 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
